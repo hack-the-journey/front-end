94e2e04b65abf120620fba200bfae0c9e669bb2e
Rahul Shah <rahulshah7@outlook.com>
2019-11-09 15:25:11 +0000

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import BucketList from './components/BucketList'
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO</h1>
       <BucketList></BucketList>
+      <Button variant="contained" color="primary">
+        Hello World
+    </Button>
     </div>
   );
 }
