741e376fbdc21128491e2ac95499a0332abc5b4b
Rahul Shah <rahulshah7@outlook.com>
2019-11-09 16:40:35 +0000

    Add delete bucket list item feature

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,36 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import bucketListService from '../services/bucketlist';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Button from '@material-ui/core/Button';
-
-
+import React, { useState, useEffect } from "react";
+import bucketListService from "../services/bucketlist";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+import bucketlist from "../services/bucketlist";
 
 function BucketList() {
-    const [bucketList, setBucketList] = useState([]);
+  const [bucketList, setBucketList] = useState([]);
 
-    useEffect(() => {
-        bucketListService.getAll().then(bucketListItems => setBucketList(bucketListItems));
-    }, []);
+  useEffect(() => {
+    bucketListService
+      .getAll()
+      .then(bucketListItems => setBucketList(bucketListItems));
+  }, []);
 
-
+  const handleDelete = id => {
+    bucketListService
+      .destroy(id)
+      .then(returnedItem =>
+        setBucketList(bucketList.filter(item => item.id !== id))
+      );
+  };
 
   return (
     <div className="App">
       <h1>BucketList</h1>
-        <List component="nav" aria-label="main mailbox folders">
-            {bucketList.map(item =>
-                <ListItem button>
-                    <ListItemText primary={item.destination} />
-                    <Button variant="contained" color="primary">
-                    X
-                    </Button>
-                </ListItem>
-            )}
-        </List>
-
+      <List component="nav" aria-label="main mailbox folders">
+        {bucketList.map(item => (
+          <ListItem key={item.id} button>
+            <ListItemText primary={item.destination} />
+            <Button
+              onClick={() => handleDelete(item.id)}
+              variant="contained"
+              color="primary"
+            >
+              X
+            </Button>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
--- src/services/bucketlist.js
+++ src/services/bucketlist.js
@@ -1,27 +1,23 @@
 import axios from "axios";
 
-let baseUrl = "http://localhost:3001";
+let baseUrl = "http://localhost:3001/bucketlist/";
 
 const getAll = () => {
-    return axios
-        .get( baseUrl + "/bucketlist")
-        .then(response => response.data);
+  return axios.get(baseUrl).then(response => response.data);
 };
 
 const create = bucketItem => {
-    return axios
-        .post( baseUrl + "/bucketlist", bucketItem)
-        .then(response => response.data);
+  return axios.post(baseUrl, bucketItem).then(response => response.data);
 };
 
-const update = (bucketItem, personId) => {
-    return axios
-        .put( baseUrl + `/bucketlist"${personId}`, bucketItem)
-        .then(response => response.data);
+const update = (bucketItem, itemId) => {
+  return axios
+    .put(baseUrl + itemId, bucketItem)
+    .then(response => response.data);
 };
 
-const destroy = personId => {
-    return axios.delete( baseUrl + `/bucketlist"/${personId}`);
+const destroy = itemId => {
+  return axios.delete(baseUrl + itemId);
 };
 
 export default { getAll, create, update, destroy };
